67d51719fafa5663c30d779cb269c5614fcda9fb
lucia <583984750@qq.com>
2025-10-06 18:56:06 +0800

    暂时取消了99A的登记

--- src/main/java/com/modernwarfare/dragonrise/init/ModEntities.java
+++ src/main/java/com/modernwarfare/dragonrise/init/ModEntities.java
@@ -1,8 +1,8 @@
 package com.modernwarfare.dragonrise.init;
 
 import com.modernwarfare.dragonrise.Mod;
+import com.modernwarfare.dragonrise.entity.AbstractVehicleEntity;
 import com.modernwarfare.dragonrise.entity.ZHI10MEEntity;
-import com.modernwarfare.dragonrise.entity.ZTZ99AEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
@@ -13,31 +13,31 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModEntities {
     public static final DeferredRegister<EntityType<?>> REGISTRY = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, Mod.MODID);
 
-    // Register Tom7
-//    public static final RegistryObject<EntityType<Tom7Entity>> TOM_7 = register("tom_7",
-//            EntityType.Builder.<Tom7Entity>of(Tom7Entity::new, MobCategory.MISC)
+//    public static final RegistryObject<EntityType<ZTZ99AEntity>> ZTZ99A = register("ztz99a",
+//            EntityType.Builder.<ZTZ99AEntity>of(ZTZ99AEntity::new, MobCategory.MISC)
 //                    .setTrackingRange(64)
 //                    .setUpdateInterval(1)
-//                    .setCustomClientFactory(Tom7Entity::new)
+//                    .setCustomClientFactory(ZTZ99AEntity::new)
 //                    .fireImmune()
-//                    .sized(1.05f, 1.0f)
+//                    .sized(4.0f, 2.9f)
 //    );
-    public static final RegistryObject<EntityType<ZTZ99AEntity>> ZTZ99A = register("ztz99a",
-            EntityType.Builder.<ZTZ99AEntity>of(ZTZ99AEntity::new, MobCategory.MISC)
-                    .setTrackingRange(64)
-                    .setUpdateInterval(1)
-                    .setCustomClientFactory(ZTZ99AEntity::new)
-                    .fireImmune()
-                    .sized(4.0f, 2.9f)
-    );
+
     public static final RegistryObject<EntityType<ZHI10MEEntity>> ZHI10ME = register("zhi10me",
             EntityType.Builder.<ZHI10MEEntity>of(ZHI10MEEntity::new,MobCategory.MISC)
                     .setTrackingRange(64)
                     .setUpdateInterval(1)
                     .setCustomClientFactory(ZHI10MEEntity::new)
                     .fireImmune()
-                    .sized(3.6f,2.9f)
+                    .sized(1.4f,2.9f)
     );
+//    public static final RegistryObject<EntityType<AbstractVehicleEntity>> VEHICLE = register("vehicle",
+//            EntityType.Builder.<AbstractVehicleEntity>of(AbstractVehicleEntity::new, MobCategory.MISC)
+//                   .setTrackingRange(64)
+//                   .setUpdateInterval(1)
+//                   .setCustomClientFactory(AbstractVehicleEntity::new)
+//                   .fireImmune()
+//                   .sized(1.0f, 1.0f)
+//            );
 
     private static <T extends Entity> RegistryObject<EntityType<T>> register(String name, EntityType.Builder<T> entityTypeBuilder) {
         return REGISTRY.register(name, () -> entityTypeBuilder.build(name));
--- src/main/java/com/modernwarfare/dragonrise/init/ModEntityRenderers.java
+++ src/main/java/com/modernwarfare/dragonrise/init/ModEntityRenderers.java
@@ -5,7 +5,6 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import com.modernwarfare.dragonrise.client.renderer.entity.ZTZ99ARenderer;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ModEntityRenderers {
@@ -15,7 +14,7 @@ public class ModEntityRenderers {
 
         // Register entity render for tom7
         //event.registerEntityRenderer(ModEntities.TOM_7.get(), Tom7Renderer::new);
-        event.registerEntityRenderer(ModEntities.ZTZ99A.get(), ZTZ99ARenderer::new);
+//        event.registerEntityRenderer(ModEntities.ZTZ99A.get(), ZTZ99ARenderer::new);
         event.registerEntityRenderer(ModEntities.ZHI10ME.get(), ZHI10MERenderer::new);
     }
 }
